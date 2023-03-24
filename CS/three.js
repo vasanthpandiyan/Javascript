64c841703ebcbed47d77c5b20f5cdae0694a1785
vasanthpandiyan <vasanthpandiyan4059648@gmail.com>
2023-03-24 12:21:58 +0530

    ok

---
+++ CS/three.js
@@ -0,0 +1,8 @@
+let number = 0
+if (number >=0)
+{
+    console.log("given number is positive")
+}
+else{
+    console.log("given number is negative")
+}
--- cs one.js
+++
@@ -1,7 +0,0 @@
-let number=63;
-if(number % 7 === 0){
-    console.log("number is divisible by 7")
-}
-else{
-    console.log("number is not divisible by 7")
-}       
