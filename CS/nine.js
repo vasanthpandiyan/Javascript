913b273e80dc1d3b1d82fa4ca98b277a1fb70ac7
vasanthpandiyan <vasanthpandiyan4059648@gmail.com>
2023-03-24 23:46:50 +0530

    ok

---
+++ CS/nine.js
@@ -0,0 +1,13 @@
+let a=34
+    b=67
+    c=45
+    if (a>b && a>C) {
+        console.log("a is the greatest number")
+    }
+    else if (b>a && b>c) {
+        console.log("b is the greatest number")
+    }
+     else {
+        console.log("c is the greatest number")
+    }
+    
