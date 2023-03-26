6a811453622f3adab558b6bfb34278d8e49179fe
vasanthpandiyan <vasanthpandiyan4059648@gmail.com>
2023-03-26 16:26:34 +0530

    ok

---
+++ CS/thirteen.js
@@ -0,0 +1,12 @@
+let a=12
+    b=35
+    c=34
+    if(a>b && a>c){
+        console.log("a is the largest number")
+    }
+    else if (b>a && b>c){
+        console.log(" b is the greatest number")
+    } 
+    else{
+        console.log("c is the greatest number")
+    }
