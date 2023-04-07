4e5c501d4c1dfb09c8f793554777502d9da55afe
vasanthpandiyan <vasanthpandiyan4059648@gmail.com>
2023-04-07 14:19:31 +0530

    ok

---
+++ Array assignments/seven.js
@@ -0,0 +1,12 @@
+// create 2D array in javascript //
+const twoDArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  for (let i = 0; i < twoDArray.length; i++) {
+    for (let j = 0; j < twoDArray[i].length; j++) {
+      console.log(twoDArray[i][j]);
+    }
+  }
+  
