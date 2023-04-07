eb6427450d1aa3e0e584ba40d812dcf0c15b261b
vasanthpandiyan <vasanthpandiyan4059648@gmail.com>
2023-04-07 14:25:44 +0530

    ok

---
+++ Array assignments/nine.js
@@ -0,0 +1,12 @@
+// to access 2D array item in javascript //
+const twoDArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  
+  // Access the item at row 1, column 2
+  const item = twoDArray[1][2];
+  
+  console.log(item);    
+  
